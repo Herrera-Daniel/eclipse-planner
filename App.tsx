92d09d0fb3c7c05231fce1d781db17c86e73b7e1
Herrera-Daniel <dan_michigan2012@hotmail.com>
2023-10-07 20:39:43 -0600

    fixed navbar themeing

--- App.tsx
+++ App.tsx
@@ -1,75 +1,84 @@
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import {
-    StyleSheet, useColorScheme
+	StyleSheet, useColorScheme
 } from 'react-native';
+import IonIcons from 'react-native-vector-icons/Ionicons';
 
+import { PaperProvider } from 'react-native-paper';
 import {
-    Colors
+	Colors
 } from 'react-native/Libraries/NewAppScreen';
 import Explore from './src/screens/Explore';
 import Home from './src/screens/Home';
 
 const options = (iconName: string) => (
-    {
-        tabBarIcon: ({ focused }: any) => (
-            <Ionicons
-                style={{ backgroundColor: '#171b25' }}
-                color={'#139A43'}
-                name={focused ? iconName : iconName + '-outline'}
-                size={24}
-            />
-        ),
-    }
+	{
+		tabBarIcon: ({ focused }: any) => (
+			<IonIcons
+				style={{borderRadius: 20, backgroundColor: '#171b25' }}
+				color={'#139A43'}
+				name={focused ? iconName : iconName + '-outline'}
+				size={24}
+			/>
+		),
+	}
 );
 
 const Tab = createMaterialBottomTabNavigator();
 
 function App(): JSX.Element {
-    const isDarkMode = useColorScheme() === 'dark';
+	const isDarkMode = useColorScheme() === 'dark';
 
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-        flex: 1,
-    };
+	const backgroundStyle = {
+		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+		flex: 1,
+	};
 
-    return (
-        <NavigationContainer>
-            <Tab.Navigator
-                initialRouteName={'Home'}
-                activeColor={'white'}
-                inactiveColor={undefined}
-                shifting={true}
-                barStyle={{
-                    backgroundColor: '#171b25',
-                }}
-            >
-                <Tab.Screen options={options('home')} name='Home' component={Home} />
-                <Tab.Screen options={options('compass')} name='Explore' component={Explore} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+	return (
+		<NavigationContainer>
+			<PaperProvider theme={theme}>
+				<Tab.Navigator
+					initialRouteName={'Home'}
+					activeColor={'white'}
+					inactiveColor={'#171b25'}
+					shifting={true}
+					barStyle={{
+						backgroundColor: '#171b25',
+					}}
+				>
+					<Tab.Screen options={options('home')} name='Home' component={Home} />
+					<Tab.Screen options={options('compass')} name='Explore' component={Explore} />
+				</Tab.Navigator>
+			</PaperProvider>
+		</NavigationContainer>
+	);
 }
 
+const theme = {
+    colors: {
+        secondaryContainer: '#171b25',
+    },
+};
+
 const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
-    },
+	sectionContainer: {
+		marginTop: 32,
+		paddingHorizontal: 24,
+	},
+	sectionTitle: {
+		fontSize: 24,
+		fontWeight: '600',
+	},
+	sectionDescription: {
+		marginTop: 8,
+		fontSize: 18,
+		fontWeight: '400',
+	},
+	highlight: {
+		fontWeight: '700',
+	},
 });
 
 export default App;
--- src/screens/Explore.tsx
+++ src/screens/Explore.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
+import { AppStyles } from "../../AppStyles";
 
 const Explore = () => {
     return (
-        <View>
-            <Text>
+        <View style={AppStyles.screenView}>
+            <Text style={AppStyles.text}>
                 This would be the explore page
             </Text>
 
