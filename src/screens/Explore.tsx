816d793372d054f3ae10fdab214875d274f3c7f2
Herrera-daniel <dan_michigan2012@hotmail.com>
2023-10-19 19:10:23 -0600

    added google maps

--- src/screens/Explore.tsx
+++ src/screens/Explore.tsx
@@ -1,13 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { AppStyles } from "../../AppStyles";
+
+const styles = StyleSheet.create({
+ container: {
+   ...StyleSheet.absoluteFillObject,
+   height: 400,
+   width: 400,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+ },
+ map: {
+   ...StyleSheet.absoluteFillObject,
+ },
+});
 
 const Explore = () => {
     return (
         <View style={AppStyles.screenView}>
-            <Text style={AppStyles.text}>
-                This would be the explore page
-            </Text>
+            <MapView
+                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                style={styles.map}
+                region={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}            >
 
+            </MapView>
         </View>
     );
 };
